906c7980f56f46ea2e55073ca7ee848b240f4117
Ryan L'Italien <1479756+ryanlitalien@users.noreply.github.com>
2019-02-26 11:36:19 -0500

    Drop dev/null to see the output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh -c "$JEST_CMD $* --ci --testLocationInResults --json --outputFile=report.json" &> /dev/null
+sh -c "$JEST_CMD $* --ci --testLocationInResults --json --outputFile=report.json"
 set -e
 sh -c "cat report.json | /usr/bin/jest-action"
